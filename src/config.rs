f11c8441b05e23dcbd9ab8e25b7122d866278ba6
jing.jin <jing.jin@zkh360.com>
2022-05-03 22:12:13 +0800

    feat: put default config to ini file

--- src/config.rs
+++ src/config.rs
@@ -1,27 +1,19 @@
+use ini::Ini;
 use std::collections::HashMap;
 use std::env;
 use std::path::Path;
 
 pub fn get_registry_config() -> HashMap<String, String> {
-    let registry_list = HashMap::from([
-        ("npm".to_string(), "https://registry.npmjs.org/".to_string()),
-        (
-            "yarn".to_string(),
-            "https://registry.yarnpkg.com/".to_string(),
-        ),
-        (
-            "tencent".to_string(),
-            "https://mirrors.cloud.tencent.com/npm/".to_string(),
-        ),
-        (
-            "taobao".to_string(),
-            "https://registry.npmmirror.com/".to_string(),
-        ),
-        (
-            "npmMirror".to_string(),
-            "https://skimdb.npmjs.com/registry/".to_string(),
-        ),
-    ]);
+    let mut registry_list = HashMap::new();
+    let conf = Ini::load_from_file("./config.ini").unwrap();
+
+    let general_section_name = "";
+    for (sec, prop) in &conf {
+        let section_name = sec.as_ref().unwrap_or(&general_section_name);
+        for (_k, v) in prop.iter() {
+            registry_list.insert(section_name.to_string(), v.to_string());
+        }
+    }
     return registry_list;
 }
 
@@ -42,4 +34,16 @@ pub fn get_registry_config_path(name: &str) -> String {
     let path_buf = Path::new(&home_path.to_owned()).join(name);
     let path = path_buf.as_path().display().to_string();
     path
+}
+
+#[cfg(test)]
+mod config_tests {
+    use super::*;
+
+    #[test]
+    fn it_get_right_config_path() {
+        env::set_var("HOME", "/Users/home");
+        let path = "/Users/home/abc";
+        assert_eq!(path, get_registry_config_path("abc"));
+    }
 }
